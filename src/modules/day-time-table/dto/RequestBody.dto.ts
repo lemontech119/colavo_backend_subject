67b811226ce66c69b0641cee22c709c5945e89e9
AhnDongwon <lemontech119@gmail.com>
2023-04-30 19:19:57 +0900

    Update(validation): validation 추가

--- src/modules/day-time-table/dto/RequestBody.dto.ts
+++ src/modules/day-time-table/dto/RequestBody.dto.ts
@@ -1,9 +1,28 @@
-export interface RequestBody {
+import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class RequestBody {
+  @IsString()
   start_day_identifier: string;
+
+  @IsString()
   timezone_identifier: string;
+
+  @IsNumber()
   service_duration: number;
+
+  @IsOptional()
+  @IsNumber()
   days?: number;
+
+  @IsOptional()
+  @IsNumber()
   timeslot_interval?: number;
+
+  @IsOptional()
+  @IsBoolean()
   is_ignore_schedule?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
   is_ignore_workhour?: boolean;
 }
